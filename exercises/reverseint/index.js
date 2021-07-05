5ef0690d48becff1df10dc30233241b40271b796
Maziar <Maziar.mod@gmail.com>
2021-07-05 16:30:29 -0400

    add first solution for integer reversal

--- exercises/reverseint/index.js
+++ exercises/reverseint/index.js
@@ -8,6 +8,9 @@
 //   reverseInt(-15) === -51
 //   reverseInt(-90) === -9
 
-function reverseInt(n) {}
+function reverseInt(n) {
+    
+    return Math.sign(n) * parseInt(n.toString().split('').reduce((reversed, char) => char + reversed , ''));
+}
 
 module.exports = reverseInt;
