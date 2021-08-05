feeaab4587c68a41129b3fe4de9b8e4b62a2b748
Maziar <Maziar.mod@gmail.com>
2021-08-05 13:57:37 -0400

    add third solution for anagrams

--- exercises/anagrams/index.js
+++ exercises/anagrams/index.js
@@ -61,4 +61,12 @@ function BuildCharMap(string) {
   return charMap;
 }
 
+function anagrams2(stringA, stringB) {
+    return cleanString(stringA) === cleanString(stringB);
+}
+
+function cleanString(str) {
+  return str.replace(/[^\w]/g, '').toLowerCase().split('').sort().join('');
+}
+
 module.exports = anagrams;
