af651b113b07848b8591f5a5484af99f36d1f7b4
Maziar <Maziar.mod@gmail.com>
2022-01-17 16:27:40 -0500

    add first solution for vowels

--- exercises/vowels/index.js
+++ exercises/vowels/index.js
@@ -7,6 +7,9 @@
 //   vowels('Why do you ask?') --> 4
 //   vowels('Why?') --> 0
 
-function vowels(str) {}
+function vowels(str) {
+  const regexp = /[aeiouAEIOU]/g;
+  return [...str.matchAll(regexp)].length;
+}
 
 module.exports = vowels;
