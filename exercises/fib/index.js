4dfb5c58999cf78687be49678d74a443892bd3b1
Maziar <Maziar.mod@gmail.com>
2022-01-24 15:07:42 -0500

    add forth  solution _recursive_memoized for Fibonacci series

--- exercises/fib/index.js
+++ exercises/fib/index.js
@@ -8,11 +8,27 @@
 // Example:
 //   fib(4) === 3
 
-function fib(n) {
+function memoized(fn) {
+  const cache = {};
+
+  return function (...args) {
+    if (cache[args]) {
+      return cache[args];
+    }
+    const result = fn.apply(this, args);
+    cache[args] = result;
+
+    return result;
+  };
+}
+
+function slowFib(n) {
   if (n < 2) return n;
 
   return fib(n - 1) + fib(n - 2);
 }
+
+const fib = memoized(slowFib);
 
 function fib1(n) {
   if (n === 0) return 0;
